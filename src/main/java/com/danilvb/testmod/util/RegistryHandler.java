2b9ac074e6ab1f2200333362b8ab28b8a0cc8bf2
Данил Бухаров <danilvbuh@gmail.com>
2020-09-14 18:23:51 +0700

    Создание первого предмета Успешно создан первый предмет ресурса "miodium"

---
+++ src/main/java/com/danilvb/testmod/util/RegistryHandler.java
@@ -0,0 +1,20 @@
+package com.danilvb.testmod.util;
+
+import com.danilvb.testmod.TestMod;
+import com.danilvb.testmod.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static void init(){
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    //Items
+    public static final RegistryObject<Item> Miodium = ITEMS.register("miodium", ItemBase::new);
+}
