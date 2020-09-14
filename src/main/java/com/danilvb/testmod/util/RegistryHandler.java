21e2d5d19a574b4a11eee0516f37746fc5ae1cd2
Данил Бухаров <danilvbuh@gmail.com>
2020-09-15 02:19:15 +0700

    Добавлен блок perekristium-а Инициализирован блок и добавлена его модель и текстура

--- src/main/java/com/danilvb/testmod/util/RegistryHandler.java
+++ src/main/java/com/danilvb/testmod/util/RegistryHandler.java
@@ -3,6 +3,7 @@ package com.danilvb.testmod.util;
 import com.danilvb.testmod.TestMod;
 import com.danilvb.testmod.blocks.BlockItemBase;
 import com.danilvb.testmod.blocks.MiodiumBlock;
+import com.danilvb.testmod.blocks.PerekristiumBlock;
 import com.danilvb.testmod.items.ItemBase;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
@@ -27,11 +28,12 @@ public class RegistryHandler {
 
     //Blocks
     public static final RegistryObject<Block> MIODIUM_BLOCK = BLOCKS.register("miodium_block", MiodiumBlock::new);
-    //public static final RegistryObject<Block> PEREKRISTIUM_BLOCK = BLOCKS.register("perekristium_block", PerekristiumBlock::new);
+    public static final RegistryObject<Block> PEREKRISTIUM_BLOCK = BLOCKS.register("perekristium_block", PerekristiumBlock::new);
 
 
     //Block Items
     public static final RegistryObject<Item> MIODIUM_BLOCK_ITEM = ITEMS.register("miodium_block", () -> new BlockItemBase(MIODIUM_BLOCK.get()));
+    public static final RegistryObject<Item> PEREKRISTIUM_BLOCK_ITEM = ITEMS.register("perekristium_block", () -> new BlockItemBase(PEREKRISTIUM_BLOCK.get()));
 
 
 }
