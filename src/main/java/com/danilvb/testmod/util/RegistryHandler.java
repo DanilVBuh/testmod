5317508bf953bc459d32c4554c3a478f8b4719d0
Данил Бухаров <danilvbuh@gmail.com>
2020-09-15 20:28:59 +0700

    Добавлен блок руды miodium-а, рецепты и луттэйблы

--- src/main/java/com/danilvb/testmod/util/RegistryHandler.java
+++ src/main/java/com/danilvb/testmod/util/RegistryHandler.java
@@ -3,9 +3,11 @@ package com.danilvb.testmod.util;
 import com.danilvb.testmod.TestMod;
 import com.danilvb.testmod.blocks.BlockItemBase;
 import com.danilvb.testmod.blocks.MiodiumBlock;
+import com.danilvb.testmod.blocks.MiodiumOre;
 import com.danilvb.testmod.blocks.PerekristiumBlock;
 import com.danilvb.testmod.items.ItemBase;
 import net.minecraft.block.Block;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
@@ -22,17 +24,21 @@ public class RegistryHandler {
         BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
+
     //Items
     public static final RegistryObject<Item> MIODIUM = ITEMS.register("miodium", ItemBase::new);
     public static final RegistryObject<Item> PEREKRISTIUM = ITEMS.register("perekristium", ItemBase::new);
 
+
     //Blocks
     public static final RegistryObject<Block> MIODIUM_BLOCK = BLOCKS.register("miodium_block", MiodiumBlock::new);
+    public static final RegistryObject<Block> MIODIUM_ORE = BLOCKS.register("miodium_ore", MiodiumOre::new);
     public static final RegistryObject<Block> PEREKRISTIUM_BLOCK = BLOCKS.register("perekristium_block", PerekristiumBlock::new);
 
 
     //Block Items
     public static final RegistryObject<Item> MIODIUM_BLOCK_ITEM = ITEMS.register("miodium_block", () -> new BlockItemBase(MIODIUM_BLOCK.get()));
+    public static final RegistryObject<Item> MIODIUM_ORE_ITEM = ITEMS.register("miodium_ore", () -> new BlockItemBase(MIODIUM_ORE.get()));
     public static final RegistryObject<Item> PEREKRISTIUM_BLOCK_ITEM = ITEMS.register("perekristium_block", () -> new BlockItemBase(PEREKRISTIUM_BLOCK.get()));
 
 
