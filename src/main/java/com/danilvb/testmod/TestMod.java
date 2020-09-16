ed003a1f9300e5f57620e2ce3cf1eabf4bd595e1
Данил Бухаров <danilvbuh@gmail.com>
2020-09-16 10:53:11 +0700

    Created miodium sword and added its texture

--- src/main/java/com/danilvb/testmod/TestMod.java
+++ src/main/java/com/danilvb/testmod/TestMod.java
@@ -1,6 +1,8 @@
 package com.danilvb.testmod;
 
 import com.danilvb.testmod.util.RegistryHandler;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -29,8 +31,16 @@ public class TestMod
 
     }
 
-    private void doClientStuff(final FMLClientSetupEvent event) {
+    private void doClientStuff(final FMLClientSetupEvent event)
+    {
 
     }
+
+    public static final ItemGroup TAB = new ItemGroup("testTab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(RegistryHandler.MIODIUM.get());
+        }
+    };
 
 }
--- src/main/java/com/danilvb/testmod/util/RegistryHandler.java
+++ src/main/java/com/danilvb/testmod/util/RegistryHandler.java
@@ -2,7 +2,9 @@ package com.danilvb.testmod.util;
 
 import com.danilvb.testmod.TestMod;
 import com.danilvb.testmod.items.ItemBase;
+import com.danilvb.testmod.tools.MiodiumItemTier;
 import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,4 +20,8 @@ public class RegistryHandler {
     //Items
     public static final RegistryObject<Item> MIODIUM = ITEMS.register("miodium", ItemBase::new);
     public static final RegistryObject<Item> PEREKRISTIUM = ITEMS.register("perekristium", ItemBase::new);
+
+    //Tools
+    public static final RegistryObject<SwordItem> MIODIUM_SWORD = ITEMS.register("miodium_sword", () ->
+            new SwordItem(MiodiumItemTier.MIODIUM, 3, -1.0F, new Item.Properties().group(TestMod.TAB)));
 }
