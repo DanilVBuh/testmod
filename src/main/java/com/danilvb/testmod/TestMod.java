c76d73f41583193e79bbb0ed673779fb068baf7e
Данил Бухаров <danilvbuh@gmail.com>
2020-09-16 20:34:10 +0700

    Добавлена текстура для моделей брони

--- src/main/java/com/danilvb/testmod/TestMod.java
+++ src/main/java/com/danilvb/testmod/TestMod.java
@@ -1,6 +1,8 @@
 package com.danilvb.testmod;
 
 import com.danilvb.testmod.util.RegistryHandler;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -32,5 +34,12 @@ public class TestMod
     private void doClientStuff(final FMLClientSetupEvent event) {
 
     }
+
+    public static final ItemGroup TAB = new ItemGroup("testTab") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(RegistryHandler.MIODIUM.get());
+        }
+    };
 
 }
--- src/main/java/com/danilvb/testmod/util/RegistryHandler.java
+++ src/main/java/com/danilvb/testmod/util/RegistryHandler.java
@@ -1,7 +1,10 @@
 package com.danilvb.testmod.util;
 
 import com.danilvb.testmod.TestMod;
+import com.danilvb.testmod.armor.TestArmorMaterial;
 import com.danilvb.testmod.items.ItemBase;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -18,4 +21,8 @@ public class RegistryHandler {
     //Items
     public static final RegistryObject<Item> MIODIUM = ITEMS.register("miodium", ItemBase::new);
     public static final RegistryObject<Item> PEREKRISTIUM = ITEMS.register("perekristium", ItemBase::new);
+
+    //Armor
+    public static final RegistryObject<ArmorItem> MIODIUM_HELMET = ITEMS.register("miodium_helmet",
+            () -> new ArmorItem(TestArmorMaterial.MIODIUM, EquipmentSlotType.HEAD, new Item.Properties().group(TestMod.TAB)));
 }
